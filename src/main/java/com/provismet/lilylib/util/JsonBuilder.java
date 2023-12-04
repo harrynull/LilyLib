97c18e2ddb777af4ac6eee3081c0aa18305c42ef
Provismet <17149901+Provismet@users.noreply.github.com>
2023-12-04 00:02:12 +0000

    Add all utils from previous mods.

---
+++ src/main/java/com/provismet/lilylib/util/JsonBuilder.java
@@ -0,0 +1,214 @@
+package com.provismet.lilylib.util;
+
+import java.util.Iterator;
+
+/**
+ * Builds JSON-formatted strings. This is a wrapper of the StringBuilder type.
+ */
+public class JsonBuilder {
+    private StringBuilder builder;
+    private int indentationLevel;
+
+    public JsonBuilder () {
+        this.builder = new StringBuilder();
+        this.indentationLevel = 0;
+    }
+
+    /**
+     * Starts the JSON object.
+     * @return this
+     */
+    public JsonBuilder start () {
+        this.builder.append("{\n");
+        this.indentationLevel++;
+        return this;
+    }
+
+    /**
+     * Adds a single string value to the JSON object.
+     * @param value The string value.
+     * @return this
+     */
+    public JsonBuilder append (String value) {
+        this.indent();
+        this.builder.append("\"").append(value).append("\"");
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     * @param key
+     * @param value
+     * @return this
+     */
+    public JsonBuilder append (String key, String value) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": ");
+        this.builder.append("\"").append(value).append("\"");
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     * @param key
+     * @param value
+     * @return this
+     */
+    public JsonBuilder append (String key, int value) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": ").append(value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     * @param key
+     * @param value
+     * @return this
+     */
+    public JsonBuilder append (String key, boolean value) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": ").append(value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     * Note that doubles and floats are indistinguishable after appending.
+     * @param key
+     * @param value
+     * @return this
+     */
+    public JsonBuilder append (String key, float value) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": ").append(value);
+        return this;
+    }
+
+    /**
+     * Adds a key-value pair to the JSON object.
+     * Note that doubles and floats are indistinguishable after appending.
+     * @param key
+     * @param value
+     * @return this
+     */
+    public JsonBuilder append (String key, double value) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": ").append(value);
+        return this;
+    }
+
+    /**
+     * Closes the current object. This should also be used to close the top-level object.
+     * @return this
+     */
+    public JsonBuilder closeObject () {
+        this.indentationLevel--;
+        this.indent();
+        this.builder.append("}");
+        return this;
+    }
+
+    /**
+     * Creates a key-value pair for a new array under the given key.
+     * @param key
+     * @return this
+     */
+    public JsonBuilder startArray (String key) {
+        this.indent();
+        this.builder.append("\"").append(key).append("\": [");
+        this.indentationLevel++;
+        return this;
+    }
+
+    /**
+     * Closes the current array.
+     * @return this
+     */
+    public JsonBuilder closeArray () {
+        this.indentationLevel--;
+        this.indent();
+        this.builder.append("]");
+        return this;
+    }
+
+    /**
+     * Creates an entire array from an iterable of String values.
+     * 
+     * This function encapsulates the starting and closing of the array.
+     * 
+     * @param key The key of the key-value pair.
+     * @param values The values of the array.
+     * @return this
+     */
+    public JsonBuilder createArray (String key, Iterable<String> values) {
+        this.startArray(key).newLine(false);
+
+        Iterator<String> iter = values.iterator();
+
+        if (iter.hasNext()) {
+            String last = iter.next();
+            
+            while (iter.hasNext()) {
+                this.append(last).newLine(true);
+                last = iter.next();
+            }
+
+            this.append(last);
+        }
+
+        this.newLine(false);
+        this.closeArray();
+        return this;
+    }
+
+    /**
+     * Creates an entire array from an undefined number of String arguments.
+     * 
+     * Uses the iterable variant as its backend.
+     * 
+     * @param key
+     * @param values
+     * @return this
+     */
+    public JsonBuilder createArray (String key, String... values) {
+        return this.createArray(key, values);
+    }
+
+    /**
+     * Creates a new line with a comma.
+     * @return this
+     */
+    public JsonBuilder newLine () {
+        return this.newLine(true);
+    }
+
+    /**
+     * Creates a new line with an optional comma.
+     * @param addComma
+     * @return this
+     */
+    public JsonBuilder newLine (boolean addComma) {
+        if (addComma) this.builder.append(",");
+        this.builder.append("\n");
+        return this;
+    }
+
+    private void indent () {
+        for (int i = 0; i < this.indentationLevel; ++i) {
+            this.builder.append("\t");
+        }
+    }
+
+    /**
+     * Builds and returns the current String held by the internal StringBuilder.
+     * 
+     * Use this to output the final JSON string.
+     * 
+     * @return The current string representation of the JSON object.
+     */
+    @Override
+    public String toString () {
+        return this.builder.toString();
+    }
+}
---
+++ src/main/java/com/provismet/lilylib/util/MoreMath.java
@@ -0,0 +1,54 @@
+package com.provismet.lilylib.util;
+
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+
+/**
+ * An additional Math-related class that contains functions not covered by MathHelper.
+ */
+public class MoreMath {
+    public static double roundDownToMultiple (double value, double denominator) {
+        return (double)MathHelper.floor(value / denominator) * denominator;
+    }
+
+    /**
+     * Allows for the creation and representation of right-angled triangles.
+     * 
+     *             end
+     *           /|
+     *          / |
+     *       h /  | a
+     *        /   |
+     *       /    |
+     *      /_____|
+     * start    o
+     */
+    public class RightAngledTriangle {
+        private final double hypotenuseLength; // Defined as the line between the start and end vectors.
+        private final double oppositeLength; // The side opposite to the end vector.
+        private final double adjacentLength; // The non-hypotenuse side adjacent to the end vector.
+
+        public RightAngledTriangle (Vec3d hypotenuseStart, Vec3d hypotenuseEnd) {
+            this.oppositeLength = clamped(hypotenuseEnd.getX() - hypotenuseStart.getX());
+            this.adjacentLength = clamped(hypotenuseEnd.getZ() - hypotenuseStart.getZ());
+            this.hypotenuseLength = Math.sqrt(this.oppositeLength * this.oppositeLength + this.adjacentLength * this.adjacentLength);
+        }
+
+        private double clamped (double value) {
+            if (value == 0) return 0.00001;
+            else return value;
+        }
+
+        public double cosine () {
+            return this.adjacentLength / this.hypotenuseLength;
+        }
+
+        public double sine () {
+            return this.oppositeLength / this.hypotenuseLength;
+        }
+
+        public double tangent () {
+            return sine() / cosine();
+        }
+    }
+}
